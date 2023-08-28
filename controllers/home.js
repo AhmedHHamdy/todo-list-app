67542efcf6b17d7aa6085bb177c1d9efbeb0d798
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-08-28 17:21:55 +0300

    fixing meta og:image tag

--- controllers/home.js
+++ controllers/home.js
@@ -5,7 +5,7 @@ module.exports = {
     res.render('index.ejs')
   },
   getImage: (req, res) => {
-    const imagePath = path.join("https://listify-todolist-app.cyclic.cloud/", '../public/assets/listify.png');
-    res.sendFile(imagePath);
+    const imageUrl = "https://listify-todolist-app.cyclic.cloud/public/assets/listify.png";
+    res.redirect(imageUrl);
   }
 }
