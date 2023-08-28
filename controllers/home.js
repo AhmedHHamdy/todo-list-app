55cc24ff5ad75c0812bad3052a4f91fccf78f478
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-08-28 17:24:10 +0300

    Fixing og meta tag issue

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,5 @@
-
-const path = require('path');
 module.exports = {
   getIndex: (req, res) => {
     res.render('index.ejs')
-  },
-  getImage: (req, res) => {
-    const imageUrl = "https://listify-todolist-app.cyclic.cloud/public/assets/listify.png";
-    res.redirect(imageUrl);
   }
 }
